0915c2a8f0651850ff2829a310f176de5598c626
Noah Birnel <nbirnel@gmail.com>
2015-09-01 13:04:15 -0700

    make bash login for rvm

--- config.h
+++ config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "bash", "--login", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
