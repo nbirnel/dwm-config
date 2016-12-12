083f357ddcabae53ec0f31dd27615cbd42b983ac
Noah Birnel <nbirnel@gmail.com>
2016-12-12 15:31:11 -0800

    update to current int standards

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const int showbar           = 1;     /* False means no bar */
+static const int topbar            = 1;     /* False means bottom bar */
 
 /* tagging */
 static const char *tags[] = {
@@ -55,7 +55,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const int resizehints = 1; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
